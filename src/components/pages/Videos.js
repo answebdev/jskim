7741b2c82aaacbffe8b567b50759582a24592b69
ANS <36783010+answebdev@users.noreply.github.com>
2023-02-20 18:48:54 -0800

    fix console error

--- src/components/pages/Videos.js
+++ src/components/pages/Videos.js
@@ -92,7 +92,7 @@ const Videos = () => {
                             src={item.videoUrl}
                             frameborder='0'
                             allow='autoplay; fullscreen; picture-in-picture'
-                            allowFullScreen
+                            // allowFullScreen
                             style={{
                               position: 'absolute',
                               top: '0',
