5eaab8174717f2a3597d02fe504f258a8d24604a
ANS <36783010+answebdev@users.noreply.github.com>
2023-03-06 18:44:01 -0800

    update video tags

--- src/components/pages/Videos.js
+++ src/components/pages/Videos.js
@@ -41,24 +41,13 @@ const Videos = () => {
         <script type='application/ld+json'>{`
         {
             "@context": "http://schema.org",
-            "@type":"VideoObject",
-            "embedUrl": "https://player.vimeo.com/video/802908574?h=35d231015f",
-            "thumbnailUrl": "https://i.vimeocdn.com/video/1620674454-3ef48c25a8d6e561b4399ff71cafcd3d38aa38bf8aa97c8746ac032f6560cdc8-d_640",
-            "name": "Standard Chartered Bank 2021 Digital Wealth",
-            "description": "Standard Chartered Bank 2021 Digital Wealth",
-            "duration": "PT72S",
-            "uploadDate": "2023-02-27 20:02:48"
-        },
-        {
-            "@context": "http://schema.org",
-            "@type":"VideoObject",
+            "@type": "VideoObject",
             "embedUrl": "https://player.vimeo.com/video/802909824?h=37fa2e9cd3",
             "thumbnailUrl": "https://i.vimeocdn.com/video/1620680322-1459affedfbaeebdab77caa0f84329fc5fd3bb0c0c051ef5308d921d51a7f2d7-d_640",
-            "name": "Osaka.mp4",
-            "description": "Osaka",
+            "name": "Osaka.mp4","description":"Osaka",
             "duration": "PT188S",
             "uploadDate": "2023-02-27 20:08:31"
-      }
+        }
     `}</script>
       </Helmet>
       <div className={classes.Container}>
