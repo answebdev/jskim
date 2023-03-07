f16cef9108b445fc701cda45d8140d1d4a940c83
ANS <36783010+answebdev@users.noreply.github.com>
2023-03-06 19:01:08 -0800

    update video tags

--- src/components/pages/Videos.js
+++ src/components/pages/Videos.js
@@ -48,7 +48,17 @@ const Videos = () => {
             "description": "Osaka",
             "duration": "PT188S",
             "uploadDate": "2023-02-27 20:08:31"
-        }
+        },
+        {
+          "@context": "http://schema.org",
+          "@type": "VideoObject",
+          "embedUrl": "https://player.vimeo.com/video/802909824?h=37fa2e9cd3",
+          "thumbnailUrl": "https://i.vimeocdn.com/video/1620680322-1459affedfbaeebdab77caa0f84329fc5fd3bb0c0c051ef5308d921d51a7f2d7-d_640",
+          "name": "Osaka.mp4",
+          "description": "Osaka",
+          "duration": "PT188S",
+          "uploadDate": "2023-02-27 20:08:31"
+      }
     `}</script>
       </Helmet>
       <div className={classes.Container}>
